09890ec2be76a5c7389f932026e7a2b14c775394
jheikkil <jaana.kristiina.heikkilae@cern.ch>
2022-03-17 16:03:05 +0100

    Restructure Elias's code; part 13

--- L1Trigger/Phase2L1GT/test/GT_vhdl_test.py
+++ L1Trigger/Phase2L1GT/test/GT_vhdl_test.py
@@ -1,10 +1,10 @@
 import FWCore.ParameterSet.Config as cms
-import GT_translationfunctions as gt
+import L1Trigger.Phase2L1GT.GT_translationfunctions as gt
 import inspect
 from GT_config_test import globtrig
 from GT_config_test import process
 
-globtrig.importjson("GT_config_to_vhdl.json")
+globtrig.importjson("L1Trigger/Phase2L1GT/python/GT_config_to_vhdl.json")
 globtrig.getParameters(process)
 globtrig.printvhdl()
 
