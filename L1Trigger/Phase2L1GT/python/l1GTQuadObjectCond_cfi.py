0268d01472e4a86ccc748be54ba2930613a6b99f
Benjamin Huber <benjamin.huber@cern.ch>
2022-07-26 11:38:50 +0200

    Add QuadObjectCond

---
+++ L1Trigger/Phase2L1GT/python/l1GTQuadObjectCond_cfi.py
@@ -0,0 +1,22 @@
+import FWCore.ParameterSet.Config as cms
+from L1Trigger.Phase2L1GT.l1GTScales import *
+
+l1GTQuadObjectCond = cms.EDFilter(
+    "L1GTQuadObjectCond",
+    scales=cms.PSet(
+        pT_lsb=cms.double(kPT_lsb),
+        phi_lsb=cms.double(kPhi_lsb),
+        eta_lsb=cms.double(kEta_lsb),
+        dZ_lsb=cms.double(kDZ_lsb),
+        #dD_lsb = cms.double(kDD_lsb),
+        beta_lsb=cms.double(kBeta_lsb),
+        mass_lsb=cms.double(kMass_lsb),
+        seed_pT_lsb=cms.double(kSeed_pT_lsb),
+        seed_dZ_lsb=cms.double(kSeed_dZ_lsb),
+        sca_sum_lsb=cms.double(kSca_sum_lsb),
+        primvertdz_lsb=cms.double(kPrimvertdz_lsb),
+        sum_pT_pv_lsb=cms.double(kSum_pT_pv_lsb),
+        pos_chg=cms.int32(kPos_chg),
+        neg_chg=cms.int32(kNeg_chg)
+    )
+)
--- L1Trigger/Phase2L1GT/test/cmssw_test_sequence_gt_only.py
+++ L1Trigger/Phase2L1GT/test/cmssw_test_sequence_gt_only.py
@@ -33,6 +33,7 @@ process.l1t_GTProducer = cms.Path(process.GTProducer)
 from L1Trigger.Phase2L1GT.l1GTSingleObjectCond_cfi import l1GTSingleObjectCond
 from L1Trigger.Phase2L1GT.l1GTDoubleObjectCond_cfi import l1GTDoubleObjectCond
 from L1Trigger.Phase2L1GT.l1GTTripleObjectCond_cfi import l1GTTripleObjectCond
+from L1Trigger.Phase2L1GT.l1GTQuadObjectCond_cfi import l1GTQuadObjectCond
 
 # Conditions
 process.singleTkEle12 = l1GTSingleObjectCond.clone(
@@ -56,9 +57,21 @@ process.tripleTkEle20TkPho18Jet11 = l1GTTripleObjectCond.clone(
     pt3_cut = cms.double(11),
 )
 
+process.quadTkEle20 = l1GTQuadObjectCond.clone(
+    col1Tag = cms.InputTag("GTProducer", "CL2 Electrons"),
+    col2Tag = cms.InputTag("GTProducer", "CL2 Electrons"),
+    col3Tag = cms.InputTag("GTProducer", "CL2 Electrons"),
+    col4Tag = cms.InputTag("GTProducer", "CL2 Electrons"),
+    pt1_cut = cms.double(20),
+    pt2_cut = cms.double(20),
+    pt3_cut = cms.double(20),
+    pt4_cut = cms.double(20),
+)
+
 process.l1t_singleTkEle12 = cms.Path(process.singleTkEle12)
 process.l1t_doubleTkEle11TkPho11 = cms.Path(process.doubleTkEle11TkPho11)
 process.l1t_tripleTkEle20TkPho18Jet11 = cms.Path(process.tripleTkEle20TkPho18Jet11)
+process.l1t_quadTkEle20 = cms.Path(process.quadTkEle20)
 
 
 # Algo bits
@@ -67,7 +80,7 @@ from L1Trigger.Phase2L1GT.l1GTAlgoChannelConfig import generate_channel_config
 process.BoardData = cms.EDAnalyzer("L1GTBoardWriter",
   outputFilename = cms.string("outputPattern"),
   channelConfig = generate_channel_config({
-        9 : {0: "l1t_tripleTkEle20TkPho18Jet11", 2 : "l1t_singleTkEle12", 65 : "l1t_doubleTkEle11TkPho11"}
+        9 : {0: "l1t_tripleTkEle20TkPho18Jet11", 2 : "l1t_singleTkEle12", 3: "l1t_quadTkEle20", 65 : "l1t_doubleTkEle11TkPho11"}
     })
 )
 
