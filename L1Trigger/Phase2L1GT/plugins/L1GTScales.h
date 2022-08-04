a9f4af3e01e4cf45dece49d2fea1a64c0a9c6631
Benjamin Huber <benjamin.huber@cern.ch>
2022-08-04 14:09:44 +0200

    Fix hw conversions

--- L1Trigger/Phase2L1GT/plugins/L1GTScales.h
+++ L1Trigger/Phase2L1GT/plugins/L1GTScales.h
@@ -43,10 +43,10 @@ namespace l1t {
     int to_hw_primvertdz(double value) const { return std::round(value / primvertdz_lsb_); };
     int to_hw_sum_pT_pv(double value) const { return std::round(value / sum_pT_pv_lsb_); };
 
-    int to_hw_RSquared(double value) const { return std::round(value / (eta_lsb_ * eta_lsb_ + phi_lsb_ * phi_lsb_)); }
+    int to_hw_RSquared(double value) const { return std::round(value / (eta_lsb_ * eta_lsb_)); }
 
-    int to_hw_InvMass(double value) const { return std::round(value / (pT_lsb_ * pT_lsb_)) * lut_scale_; }
-    int to_hw_TransMass(double value) const { return std::round(value / (pT_lsb_ * pT_lsb_)) * lut_scale_; }
+    int to_hw_InvMass(double value) const { return std::round(value * value / (2 * pT_lsb_ * pT_lsb_)) * lut_scale_; }
+    int to_hw_TransMass(double value) const { return std::round(value * value / (2 * pT_lsb_ * pT_lsb_)) * lut_scale_; }
 
     double pT_lsb() const { return pT_lsb_; }
     double phi_lsb() const { return phi_lsb_; }
