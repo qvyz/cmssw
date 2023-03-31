a36104f5d5aa7c00481525e53e03064de7c699a4
jheikkil <jaana.kristiina.heikkilae@cern.ch>
2023-03-31 18:34:54 +0200

    Updated menu

--- L1Trigger/Phase2L1GT/python/l1tGTMenu_cfi.py
+++ L1Trigger/Phase2L1GT/python/l1tGTMenu_cfi.py
@@ -1,27 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 ############################################################
-# L1 Global Trigger Emulation
+# L1 Global Trigger Menu
 ############################################################
 
-# Conditions
+from L1Trigger.Phase2L1GT.l1tGTMenu_lepSeeds_cfi import *
 
-from L1Trigger.Phase2L1GT.l1tGTProducer_cff import l1tGTProducer
+from L1Trigger.Phase2L1GT.l1tGTMenu_hadr_metSeeds_cfi import *
 
-from L1Trigger.Phase2L1GT.l1tGTSingleObjectCond_cfi import l1tGTSingleObjectCond
-from L1Trigger.Phase2L1GT.l1tGTDoubleObjectCond_cfi import l1tGTDoubleObjectCond
-from L1Trigger.Phase2L1GT.l1tGTTripleObjectCond_cfi import l1tGTTripleObjectCond
-from L1Trigger.Phase2L1GT.l1tGTQuadObjectCond_cfi import l1tGTQuadObjectCond
-
-from L1Trigger.Phase2L1GT.l1tGTAlgoBlockProducer_cff import algorithms
-
-####### SEED 1 ###########
-
-SingleTkMuon22 = l1tGTSingleObjectCond.clone(
-    tag =  cms.InputTag("l1tGTProducer","GMTTkMuons"),
-    minPt = cms.double(22),
-    minEta = cms.double(-2.4),
-    maxEta = cms.double(2.4)
-)
-pSingleTkMuon22 = cms.Path(SingleTkMuon22)
-algorithms.append(cms.PSet(expression = cms.string("pSingleTkMuon22")))
